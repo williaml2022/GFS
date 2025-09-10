1d6cb2af46fe9df3b1a56509801bf9cece98babf
wleland2022 <william.leland@student.fairfield.edu>
2025-09-10 17:54:28 -0400

    some equations

--- Modeling_Project_1.py
+++ Modeling_Project_1.py
@@ -1,1 +1,19 @@
-print("hello")
+import math
+import numpy as np
+from scipy.integrate import quad
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+from pylab import imshow, gray, show
+from numpy import empty
+
+x = np.zeros
+y = np.zeros
+z = np.zeros
+r = np.zeros
+
+
+Dx_t = -3(x - y)
+
+Dy_t = -x*z + r*x - y
+
+Dz_t = x*y - z
