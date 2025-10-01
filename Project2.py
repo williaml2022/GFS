3a3854aa7ecffbee526235861e54092ca9c33ba9
wleland2022 <william.leland@student.fairfield.edu>
2025-10-01 16:33:17 -0400

    f fixed (graph fixed?)

--- Project2.py
+++ Project2.py
@@ -1,7 +1,3 @@
-ρ_o = 1.2#kg/m^3: reference density for the atmosphere
-#41.6 degrees north latittude
-
-
 import math
 import numpy as np
 from scipy.integrate import quad
@@ -10,25 +6,24 @@ from scipy.integrate import quad
 from pylab import imshow,gray,show
 from numpy import empty
 
-fig = plt.figure()
-#ax = plt.axes(projection ='3d')
+ρ_o = 1.2#kg/m^3: reference density for the atmosphere
+#41.6 degrees north latittude
 
-N=10000
 x = np.loadtxt("x.csv", delimiter = ",") #x.csv
 y = np.loadtxt("y.csv", delimiter = ",") #y.csv
 p = np.loadtxt("p.csv", delimiter = ",") #p.csv
-dpdx=np.zeros_like(p)
-dpdy=np.zeros_like(p)
+dpdx = np.zeros_like(p)
+dpdy = np.zeros_like(p)
 u_g = np.zeros_like(p) #geostrophic x velocity
 v_g = np.zeros_like(p) #geostrophic y velocity
-f = 3 #Coriolis Parameter
+f = 1E-4 #Coriolis Parameter
 
 
 dpdx, dpdy = np.gradient(p, x[:], y[:])
 u_g = -1/ρ_o/f * dpdx
 v_g = 1/ρ_o/f * dpdy
 
-
+#Part 1
 cp = plt.contourf(x, y, p, cmap='coolwarm') 
 plt.colorbar(cp, label = 'Pressure (Pa)')
 plt.title('Pressure Field colorized')
