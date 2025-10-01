e03d9f7b3cf058b0f6bde520e0186589ff6b93ee
wleland2022 <william.leland@student.fairfield.edu>
2025-10-01 15:23:42 -0400

    files in

--- Project2.py
+++ Project2.py
@@ -9,21 +9,21 @@ import matplotlib.pyplot as plt
 from scipy.integrate import quad
 from pylab import imshow,gray,show
 from numpy import empty
-import pandas as pd
 
 fig = plt.figure()
 #ax = plt.axes(projection ='3d')
 
 N=10000
-x = pd.read_csv("x.csv", header = None).values
-y = pd.read_csv("y.csv", header = None).values
-p = pd.read_csv("p.csv", header = None).values
-dpdx=np.zeros(N)
-dpdy=np.zeros(N)
-f = empty #Coriolis Parameter
+x = np.loadtxt("x.csv", delimiter = ",") #x.csv
+y = np.loadtxt("y.csv", delimiter = ",") #y.csv
+p = np.loadtxt("p.csv", delimiter = ",") #p.csv
+print(p[0][1])
+# dpdx=np.zeros(N)
+# dpdy=np.zeros(N)
+# f = empty #Coriolis Parameter
 
-for i in range(1,N):
-    dpdx[i]=empty
-    dpdy[i]=empty
-    u_g = -1/ρ_o/f * dpdx #geostophic x velocity
-    v_g = 1/ρ_o/f * dpdy #geostrophic y velocity
+# for i in range(1,N):
+#     dpdx[i]=empty
+#     dpdy[i]=empty
+#     u_g = -1/ρ_o/f * dpdx #geostrophic x velocity
+#     v_g = 1/ρ_o/f * dpdy #geostrophic y velocity
