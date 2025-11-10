c474420e952077407bc31d73b4e557e39fd2c3c1
wleland2022 <william.leland@student.fairfield.edu>
2025-11-10 17:24:44 -0500

    project 5 Part 1 started

--- project5.py
+++ project5.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D #3D plot
 
-y_range = z_range = 100
-z_hat = np.array([0, 0, 1])
-f= 10**-4
+x_range = y_range = 100 # km
+z_range = 4 # km
+f= 10**-4 # s^-1
 beta = 10**-11
 rho_b = 10**3
 
+z_hat = np.array([0, 0, 1])
 
 def tau_x_function(y_range):
     y_values = np.arange(0, y_range + 1)
@@ -15,34 +17,52 @@ def tau_x_function(y_range):
     return tau_x, y_values
 windStress, depth = tau_x_function(y_range)
 
-def Mek(z_range, windStress):
+
+def Mek():
     windStress_vecs = np.column_stack((windStress, np.zeros_like(windStress), np.zeros_like(windStress)))
     mass = np.cross(windStress_vecs, z_hat)/f
+
     print(mass)
     return mass
-massTransport = Mek(z_range, windStress)
-
-    
-
-# for y in range(y_range + 1):
-#     tau_x = tau_x_function(y)
-#     print(tau_x)
-#     print(y)
+massTransport = Mek() #[:, 1] means to take all rows(all depth values), and to only use first column (massTransport_y)
+massTransport_y = massTransport[:, 1]
 
 
+#Plot Wind Stress
 plt.figure()
 plt.plot(windStress, depth, marker = 'o', markersize = 4)
 plt.gca().invert_yaxis()
 plt.xlabel('Wind Stress ($N/m^2$)')
 plt.ylabel('Depth ($km$)')
-plt.title('Wind Stress Over Depth')
+plt.title('Wind Stress over Depth')
 plt.show()
 
+
+#Plot Mass Transport
 plt.figure()
-plt.plot(massTransport[:, 1], depth, marker = 'o', markersize = 4)
-plt.gca().invert_yaxis()
-plt.xlabel('Wind Stress ($N/m^2$)')
+plt.plot(massTransport_y, depth, marker = 'o', markersize = 4)
+plt.gca().invert_yaxis() #invert y axis (for accurate depth)
+plt.xlabel('Mass Transport ($kg/m/s$)')
 plt.ylabel('Depth ($km$)')
-plt.title('Mass Transport Over the z-direction')
+plt.title('Mass Transport Over Depth')
 plt.show()
+
+
+
+
+
+#3D plot of both Wind Stress & Mass Transport
+
+depth_z = np.linspace(0, z_range, len(depth))
+
+ax = plt.figure().add_subplot(111, projection = '3d')
+ax.plot(windStress, np.zeros_like(depth), depth_z, label = 'Wind Stress')
+ax.plot(np.zeros_like(depth), massTransport_y, depth_z, label = 'Ekman Transport')
+ax.set_xlabel('Wind Stress ($N/m^2$)')
+ax.set_ylabel('Ekman Transport ($kg/m/s$)')
+ax.set_zlabel('Depth ($km$)')
+plt.legend()
+plt.show()
+
+
 
