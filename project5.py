d84607954a08bd07f017ec14fa26a0ab0b1983da
wleland2022 <william.leland@student.fairfield.edu>
2025-11-17 15:10:28 -0500

    Merge conflict fix

--- project5.py
+++ project5.py
@@ -29,21 +29,13 @@ massTransport_y = Mek() #[:, 1] means only consider y direction
 #massTransport_y = massTransport[:, 1]
 
 def Psi():
-<<<<<<< HEAD
     dTau_dy = -1 * np.gradient(windStress, width) # this is del cross Tau, which is in z direction
-=======
-    dTxdy = -1 * np.gradient(windStress, width) # this is del cross Tau, which is in z direction
->>>>>>> 00bca7e5270056b5cc78ae979eba6f62253060fd
     # units of N/m^3
     x_vals = np.arange(0, x_range + 1)
     Psi_vals = np.empty(shape = (len(x_vals), len(width)))
     for i in range(len(x_vals)):
         for j in range(len(width)):
-<<<<<<< HEAD
             dT_val = dTau_dy[j] # find the value of dT/dy at the current y value
-=======
-            dT_val = dTxdy[j] # find the value of dT/dy at the current y value
->>>>>>> 00bca7e5270056b5cc78ae979eba6f62253060fd
             # because this dT_val is constant across x, we can take it out of the integral
             Psi_vals[i, j] = 1 / (rho_b * beta) * dT_val * (x_vals[i] - x_vals[0]) # and just multiply by the x distance
     
@@ -53,11 +45,7 @@ length, Psi_vals = Psi()
 Psi_vals = Psi_vals
 X, Y = np.meshgrid(length, width, indexing='ij')
 V, U = np.gradient(Psi_vals, length, width) # vector components
-<<<<<<< HEAD
 U = -U
-=======
-#U = -1*U
->>>>>>> 00bca7e5270056b5cc78ae979eba6f62253060fd
 
 # plot stream function with some [U, V] vectors overlayed
 plt.figure()
